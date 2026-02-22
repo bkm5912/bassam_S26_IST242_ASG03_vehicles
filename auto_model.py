d82c221d815685c2d31372d5c8544d2f327c937c
bkm5912 <sam@lehighradio.com>
2026-02-22 14:28:40 -0500

    Added AutoModel class and properties/getters

---
+++ auto_model.py
@@ -0,0 +1,41 @@
+'''
+Years the models are produced
+'''
+
+class AutoModel:
+    """
+    creates automodel class
+    """
+    def __init__(self, name : str, in_production : bool, years : list[int]):
+        # Name of automodel
+        self._name = name
+        # If it's still in production
+        self._in_production = in_production
+        # Years of production
+        self._years = list(years)
+
+    # Properties
+    @property
+    def get_name(self):
+        """
+        Returns the name
+        """
+        return self._name
+    
+    @property
+    def get_in_production(self):
+        """
+        Returns if it's still in production
+        """
+        return self._in_production
+    
+    @property
+    def get_years(self):
+        """
+        Returns the years of production
+        """
+        return list(self._years)
+    
+    def __str__(self):
+        return f"{self._name} inproduction = {self._in_production}"
+        f"  release years = {self._years}"
