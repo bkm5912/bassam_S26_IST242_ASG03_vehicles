b32c29645ef67a1aaf7a63498e9b73e22e63b8fb
bkm5912 <sam@lehighradio.com>
2026-03-04 14:55:52 -0500

    Add garage class

--- garage.py
+++ garage.py
@@ -1,3 +1,27 @@
+from vehicle import Vehicle
+
 """
 This is a private list of vehicles
 """
+
+class Garage:
+    # Create the constructor
+    def __init__(self):
+        self._vehicles: list[Vehicle] = []
+
+    # Create the getter
+    @property
+    def vehicles(self) -> list[Vehicle]:
+        return list[self._vehicles]
+
+    def add_vehicle(self, vehicles: Vehicle) -> None:
+        """
+        Add a vehicle to the list
+        """
+        self._vehicles.append(Vehicle)
+
+    def empty_garage(self):
+        """
+        Empty the garage of all the vehicles
+        """
+        self._vehicles.clear()
