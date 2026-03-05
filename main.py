186fbbd0b20854386a84e53de7ac649f5de74f66
bkm5912 <sam@lehighradio.com>
2026-03-05 13:46:16 -0500

    Fixed vehicles in main.py, added printing in order
    Must call add_vehicles in correct order, even if
    vehicles are created in a different order

--- main.py
+++ main.py
@@ -6,37 +6,44 @@ from auto_model import AutoModel
 from sedan import Sedan
 from truck import Truck
 from garage import Garage
+
 def main():
 
     # Trucks
     f150 = Truck(Manufacturer("Ford", "USA"),
-                 AutoModel("F150", True, list[range(2020, 2022)], 20.0)
+                 AutoModel("F150", True, list(range(2020, 2022)), 20.0)
                  )
     tundra = Truck(Manufacturer("Toyota", "Japan"),
-                 AutoModel("Tundra", False, list[range(1987, 1988)], 30.0, is_dually = True)
+                 AutoModel("Tundra", False, list(range(1987, 1988)), 30.0, is_dually = True)
                  )
 
     # Sedans
-    s = Sedan(
-        Manufacturer("Honda", "Japan"),
-            AutoModel("Civic", False, []),
-            28
-    )
-
     civic = Sedan(
         Manufacturer("Honda", "Japan"),
-            AutoModel("Civic", False, list[range(1990, 1998)]),
+            AutoModel("Civic", False, list(range(1990, 1998))),
+            28
+    )
+    m3 = Sedan(
+        Manufacturer("BMW", "Germany"),
+            AutoModel("M3 Limited", False, list(range(1990, 1998))),
             28
     )
 
-    m3 = a
-    print(s.how_far_with(10))
-
-    g = Garage()
+    g = Garage() # Empty list initiated
     g.add_vehicle(f150)
     g.add_vehicle(civic)
     g.add_vehicle(m3)
     g.add_vehicle(tundra)
+
+    print("Before sorting:")
+    print(g)
+    print()
+
+    g.sort_by_release_year()
+
+    print("After sorting:")
+    print(g)
+
 
 
 
